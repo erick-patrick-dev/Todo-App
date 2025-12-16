1cc9019eac5bdc88e5762139632185c471bae33b
Homura-san <erickbispo@ufba.br>
2025-12-15 22:42:00 -0300

    Componentes Grid e IconButton

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Grid from "../template/grid.jsx";
+import IconButton from "../template/iconButton.jsx";
 
 export default props => (
-    <div>
-        <h1>form</h1>
+
+    
+    <div role="form" className="todoForm">
+        <Grid cols="12 9 10">
+            <input id="description" className="form-control" 
+                placeholder="Adicione uma tarefa"/>
+        </Grid>
+
+        <Grid cols="col-xs-12 col-sm-3 col-md-2">
+            <IconButton style='primary' icon='plus' /> 
+        </Grid>
     </div>
 )
